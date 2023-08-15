5e923bb5b80b965d9fe9d326b6041b1b05ae5925
Max Fatouros <max.fatouros@gmail.com>
2023-08-15 10:20:49 -0700

    Initial commit

---
+++ utils/mplstyles/__init__.py
@@ -0,0 +1,17 @@
+"""
+https://matplotlib.org/stable/tutorials/introductory/customizing.html#defining-your-own-style
+"""
+from custom.utils.paths import MPLSTYLES_DIR
+
+
+
+POSTER = [
+    'seaborn-v0_8',
+    MPLSTYLES_DIR / 'paper.mplstyle',
+    MPLSTYLES_DIR / 'poster.mplstyle',
+]
+
+PAPER = [
+    'seaborn-v0_8',
+    MPLSTYLES_DIR / 'paper.mplstyle',
+]
---
+++ utils/utils/paths.py
@@ -0,0 +1,5 @@
+import custom
+from pathlib import Path
+
+ROOT_DIR = Path(custom.__path__[0])
+MPLSTYLES_DIR = ROOT_DIR / 'mplstyles'
